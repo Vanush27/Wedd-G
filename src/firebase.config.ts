0cf8a2db26e9e25825338ca1c3e0f5e227965104
vanush.grigoryan <vanushgrigoryan90@gmail.com>
2026-04-16 18:02:01 +0400

    change api KEY

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -6,13 +6,13 @@ import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  authDomain: "wedding-e3f7b.firebaseapp.com",
+  databaseURL: "https://wedding-e3f7b-default-rtdb.firebaseio.com",
+  projectId: "wedding-e3f7b",
+  storageBucket: "wedding-e3f7b.firebasestorage.app",
+  messagingSenderId: "68395348813",
+  appId: "1:68395348813:web:3a58d6cbaf9efef8a5fd04",
+  measurementId: "G-BF3VF83P7P",
 };
 
 const app = initializeApp(firebaseConfig);
